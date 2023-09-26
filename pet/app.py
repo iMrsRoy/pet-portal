fe59dc19981e909afa1879f696f0c3cb0f5dc08d
Rajarshi Roy ♠ <10099786+iMrsRoy@users.noreply.github.com>
2023-09-26 18:53:44 -0400

    all the pages are working

--- pet/app.py
+++ pet/app.py
@@ -105,60 +105,65 @@ def dog_movies():
             return render_template('dog_movies.html', petname=petname)
     
 
-choices = [
-    {
-        "name": "Tug-War",
-        "img": "https://www.everypaw.com/.imaging/mte/everypaw/590x330/dam/dog-breed-guides/cavapoo/cavapoo-2.jpg/jcr:content/cavapoo-2.jpg"
-    },
-    {
-        "name": "Belly-Rubs",
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0Lep6KiEVa6HtATMbK96Ehz8QpGYhop7lMg&usqp=CAU"
-    },
-    {
-        "name": "Feed-Treats",
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbf_t_GtIRvcqZnY87jYj7Zafe-ILfO05A-ODxyAP-OocD55w7qoGCg66aO3zCgTLahLM&usqp=CAU"
-    }
-]
-
-# Function to generate the computer's choice
-def generate_computer_choice():
-    return random.choice(choices)
-
-# Function to determine the game result
-def determine_winner(user_choice, computer_choice):
-    if user_choice["name"] == computer_choice["name"]:
-        return "It's a tie!"
-    elif (
-        (user_choice["name"] == "Tug-War" and computer_choice["name"] == "Feed-Treats") or
-        (user_choice["name"] == "Belly-Rubs" and computer_choice["name"] == "Tug-War") or
-        (user_choice["name"] == "Feed-Treats" and computer_choice["name"] == "Belly-Rubs")
-    ):
-        return "You win!"
-    else:
-        return "Computer wins!"
 
 
 @app.route('/dog_games', methods=['GET', 'POST'])
 def dog_games():
+    petname = None  # Default petname
+    choices = [
+        {
+            "name": "Tug-War",
+            "img": "https://www.everypaw.com/.imaging/mte/everypaw/590x330/dam/dog-breed-guides/cavapoo/cavapoo-2.jpg/jcr:content/cavapoo-2.jpg"
+        },
+        {
+            "name": "Belly-Rubs",
+            "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0Lep6KiEVa6HtATMbK96Ehz8QpGYhop7lMg&usqp=CAU"
+        },
+        {
+            "name": "Feed-Treats",
+            "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbf_t_GtIRvcqZnY87jYj7Zafe-ILfO05A-ODxyAP-OocD55w7qoGCg66aO3zCgTLahLM&usqp=CAU"
+        }
+    ]
+
     if request.method == 'POST':
+        user_id = session.get('user_id')
+        if user_id:
+            # If the user is logged in, retrieve their data (e.g., petname) from your data store
+            user = crud.get_user_by_id(user_id)
+            petname = user.petname
+
+        def generate_computer_choice():
+            return random.choice(choices)
+
+        def determine_winner(user_choice, computer_choice):
+            if user_choice["petname"] == computer_choice["petname"]:
+                return "It's a tie!"
+            elif (
+                (user_choice["petname"] == "Tug-War" and computer_choice["name"] == "Feed-Treats") or
+                (user_choice["petname"] == "Belly-Rubs" and computer_choice["name"] == "Tug-War") or
+                (user_choice["petname"] == "Feed-Treats" and computer_choice["name"] == "Belly-Rubs")
+            ):
+                return "You win!"
+            else:
+                return "Computer wins!"
+
         # Handle the game logic here based on user's choice
         user_choice_name = request.form.get('user_choice')
         user_choice = next((c for c in choices if c["name"] == user_choice_name), None)
         computer_choice = generate_computer_choice()
 
-        # Determine the game result based on user_choice and computer_choice
-        game_result = determine_winner(user_choice, computer_choice)
+        if user_choice is not None:
+            # Determine the game result based on user_choice and computer_choice
+            game_result = determine_winner(user_choice, computer_choice)
+        else:
+            game_result = "Please select a choice."
 
         # Pass the choices list and game_result to the template context
-        return render_template('dog_games.html', game_result=game_result, choices=choices)
+        return render_template('dog_games.html', game_result=game_result, choices=choices, petname=petname)
 
-    # Check if the user is logged in by looking for their user_id in the session
-    user_id = session.get('user_id')
-    if user_id:
-        # If the user is logged in, retrieve their data (e.g., petname) from your data store
-        user = crud.get_user_by_id(user_id)
-        petname = user.petname
-        return render_template('dog_games.html', choices=choices, petname=petname)
+    # Handle the "GET" request case (when the user navigates to the page)
+    return render_template('dog_games.html', choices=choices, petname=petname)
+
 
 
 
